9ccb255f21765b985797e0244d4df8d6ab23576c
Somaye <srn1358@gmail.com>
2021-10-17 18:17:34 -0700

    Final repository

--- CLI_project_1.py
+++ CLI_project_1.py
@@ -10,78 +10,78 @@ import cbpro
 # Using the public client of Coinbase to collect data
 c = cbpro.PublicClient()
 # Getting historical data of Bitcoin price and putting it into data frame format in Pandas
-Bitcoin = pd.DataFrame(c.get_product_historic_rates('BTC-USD', start = '2021-01-01T01:00:00', end = date.today() , granularity = 86400))
-Bitcoin.columns= ["Date","Open","High","Low","Close","Volume"]
-Bitcoin['Date'] = pd.to_datetime(Bitcoin['Date'], unit='s')
-Bitcoin.set_index('Date', inplace=True)
-Bitcoin.sort_values(by='Date', ascending=True, inplace=True)
+bitcoin = pd.DataFrame(c.get_product_historic_rates('BTC-USD', start = '2021-01-01T01:00:00', end = date.today() , granularity = 86400))
+bitcoin.columns= ["Date","Open","High","Low","Close","Volume"]
+bitcoin['Date'] = pd.to_datetime(bitcoin['Date'], unit='s')
+bitcoin.set_index('Date', inplace=True)
+bitcoin.sort_values(by='Date', ascending=True, inplace=True)
 # Getting historical data of Uniswap price and putting it into data frame format in Pandas
-Uniswap= pd.DataFrame(c.get_product_historic_rates('UNI-USD', start = '2021-01-01T01:00:00', end = date.today() , granularity = 86400))
-Uniswap.columns= ["Date","Open","High","Low","Close","Volume"]
-Uniswap['Date'] = pd.to_datetime(Uniswap['Date'], unit='s')
-Uniswap.set_index('Date', inplace=True)
-Uniswap.sort_values(by='Date', ascending=True, inplace=True)
+uniswap= pd.DataFrame(c.get_product_historic_rates('UNI-USD', start = '2021-01-01T01:00:00', end = date.today() , granularity = 86400))
+uniswap.columns= ["Date","Open","High","Low","Close","Volume"]
+uniswap['Date'] = pd.to_datetime(uniswap['Date'], unit='s')
+uniswap.set_index('Date', inplace=True)
+uniswap.sort_values(by='Date', ascending=True, inplace=True)
 # Getting historical data of Steller price and putting it into data frame format in Pandas
-Steller=  pd.DataFrame(c.get_product_historic_rates('XLM-USD', start = '2021-01-01T01:00:00', end = date.today() , granularity = 86400))
-Steller.columns= ["Date","Open","High","Low","Close","Volume"]
-Steller['Date'] = pd.to_datetime(Steller['Date'], unit='s')
-Steller.set_index('Date', inplace=True)
-Steller.sort_values(by='Date', ascending=True, inplace=True)
+steller=  pd.DataFrame(c.get_product_historic_rates('XLM-USD', start = '2021-01-01T01:00:00', end = date.today() , granularity = 86400))
+steller.columns= ["Date","Open","High","Low","Close","Volume"]
+steller['Date'] = pd.to_datetime(steller['Date'], unit='s')
+steller.set_index('Date', inplace=True)
+steller.sort_values(by='Date', ascending=True, inplace=True)
 # Getting historical data of Ethereum price and putting it into data frame format in Pandas
-Ethereum= pd.DataFrame(c.get_product_historic_rates('ETH-USD', start = '2021-01-01T01:00:00', end = date.today() , granularity = 86400))
-Ethereum.columns= ["Date","Open","High","Low","Close","Volume"]
-Ethereum['Date'] = pd.to_datetime(Ethereum['Date'], unit='s')
-Ethereum.set_index('Date', inplace=True)
-Ethereum.sort_values(by='Date', ascending=True, inplace=True)
+ethereum= pd.DataFrame(c.get_product_historic_rates('ETH-USD', start = '2021-01-01T01:00:00', end = date.today() , granularity = 86400))
+ethereum.columns= ["Date","Open","High","Low","Close","Volume"]
+ethereum['Date'] = pd.to_datetime(ethereum['Date'], unit='s')
+ethereum.set_index('Date', inplace=True)
+ethereum.sort_values(by='Date', ascending=True, inplace=True)
 # Getting historical data of Litecoin price and putting it into data frame format in Pandas
-Litecoin= pd.DataFrame(c.get_product_historic_rates('LTC-USD', start = '2021-01-01T01:00:00', end = date.today() , granularity = 86400))
-Litecoin.columns= ["Date","Open","High","Low","Close","Volume"]
-Litecoin['Date'] = pd.to_datetime(Litecoin['Date'], unit='s')
-Litecoin.set_index('Date', inplace=True)
-Litecoin.sort_values(by='Date', ascending=True, inplace=True)
+litecoin= pd.DataFrame(c.get_product_historic_rates('LTC-USD', start = '2021-01-01T01:00:00', end = date.today() , granularity = 86400))
+litecoin.columns= ["Date","Open","High","Low","Close","Volume"]
+litecoin['Date'] = pd.to_datetime(litecoin['Date'], unit='s')
+litecoin.set_index('Date', inplace=True)
+litecoin.sort_values(by='Date', ascending=True, inplace=True)
 #Concatinating five cryptocurrencies into one dataframe to start doing analysis on them.
-Five_Crypto= pd.concat([Bitcoin, Uniswap, Steller, Ethereum, Litecoin ], axis=1, keys= ["Bitcoin", "Uniswap", "Steller", "Ethereum", "Litecoin"])
+five_Crypto= pd.concat([bitcoin, uniswap, steller, ethereum, litecoin], axis=1, keys= ["Bitcoin", "Uniswap", "Steller", "Ethereum", "Litecoin"])
 # Slicing the closing price out of each dataframe
-BTC=Bitcoin.loc[:,'Close']
-UNI=Uniswap.loc[:,'Close']
-XLM= Steller.loc[:,'Close']
-ETH=Ethereum.loc[:,'Close']
-LTC= Litecoin.loc[:,'Close']
+btc=bitcoin.loc[:,'Close']
+uni=uniswap.loc[:,'Close']
+xlm= steller.loc[:,'Close']
+eth=ethereum.loc[:,'Close']
+ltc= litecoin.loc[:,'Close']
 #Adding all the closing prices to make one dataframe with five columns of closing prices and then titling each column with the relevant coin name.
-Five_Crypto_ClosingPrice=pd.concat([BTC,UNI,XLM,ETH,LTC],axis=1)
-Five_Crypto_ClosingPrice.columns=['BTC','UNI','XLM','ETH','LTC']
+five_crypto_closingprice=pd.concat([btc,uni,xlm,eth,ltc],axis=1)
+five_crypto_closingprice.columns=['BTC','UNI','XLM','ETH','LTC']
 # finding the percentage of change in the closing price for each coin.
-Five_Crypto_daily_returns= Five_Crypto_ClosingPrice.pct_change().dropna()
+five_crypto_daily_returns= five_crypto_closingprice.pct_change().dropna()
 # Finding cumulative return of the price on each coin
-Five_Crypto_cumulative_returns=(1+Five_Crypto_daily_returns).cumprod()
-User_info_Cumulative_return=Five_Crypto_cumulative_returns.tail(1)
+five_crypto_cumulative_returns=(1+five_crypto_daily_returns).cumprod()
+user_info_cumulative_return=five_crypto_cumulative_returns.tail(1)
 #getting the percentage of change in each coin and showing them to the user.
-All_Cumulative_return=User_info_Cumulative_return.values[0]*100
-print(f"Here is the cumulative return of five popular cryptocurrencies from the beginning of 2021 \n - Bitcoin: {All_Cumulative_return[0]:.1f}%." 
-     f"\n- Uniswap: {All_Cumulative_return[1]:.1f}%."
-     f"\n-Steller:{All_Cumulative_return[2]:.1f}%." 
-     f"\n-Ethereum: {All_Cumulative_return[3]:.1f}%."
-     f" \n-Litecoin: {All_Cumulative_return[4]:.1f}%."
+all_cumulative_return=user_info_cumulative_return.values[0]*100
+print(f"Here is the cumulative return of five popular cryptocurrencies from the beginning of 2021 \n - Bitcoin: {all_cumulative_return[0]:.1f}%." 
+     f"\n- Uniswap: {all_cumulative_return[1]:.1f}%."
+     f"\n-Steller:{all_cumulative_return[2]:.1f}%." 
+     f"\n-Ethereum: {all_cumulative_return[3]:.1f}%."
+     f" \n-Litecoin: {all_cumulative_return[4]:.1f}%."
      f"\n The higher the percentage of the cumulative return, the higher the investment profit")
 
-trading_days=Five_Crypto_daily_returns.count()[1]+1
-Five_crypto_Annual_average_return=Five_Crypto_daily_returns.mean()*trading_days
-Five_Crypto_Annual_std=Five_Crypto_daily_returns.std()* np.sqrt(trading_days)
-Five_crypto_Sharpe_ratios=Five_crypto_Annual_average_return/Five_Crypto_Annual_std
+trading_days=five_crypto_daily_returns.count()[1]+1
+five_crypto_annual_average_return=five_crypto_daily_returns.mean()*trading_days
+five_crypto_annual_std=five_crypto_daily_returns.std()* np.sqrt(trading_days)
+five_crypto_sharpe_ratios=five_crypto_annual_average_return/five_crypto_annual_std
 print("......................")
-print(f"Here is the Sharpe ration of the above five popular cryptocurrencies \n-Bitcoin: {Five_crypto_Sharpe_ratios[0]:.1f}." 
-     f"\n-Uniswap: {Five_crypto_Sharpe_ratios[1]:.1f}."
-     f"\n-Steller:{Five_crypto_Sharpe_ratios[2]:.1f}." 
-     f"\n-Ethereum: {Five_crypto_Sharpe_ratios[3]:.1f}."
-     f" \n-Litecoin: {Five_crypto_Sharpe_ratios[4]:.1f}."
+print(f"Here is the Sharpe ration of the above five popular cryptocurrencies \n-Bitcoin: {five_crypto_sharpe_ratios[0]:.1f}." 
+     f"\n-Uniswap: {five_crypto_sharpe_ratios[1]:.1f}."
+     f"\n-Steller:{five_crypto_sharpe_ratios[2]:.1f}." 
+     f"\n-Ethereum: {five_crypto_sharpe_ratios[3]:.1f}."
+     f" \n-Litecoin: {five_crypto_sharpe_ratios[4]:.1f}."
      f"\nThe lower the Sharpe ratio, the lower the investment risk")
 #Getting user preference over the coins to invest in.
 print ("Select two coins from this list:")
 first_coin=questionary.rawselect(
-    "Select your first coin:",choices=["Bitcoin", "Uniswap", "Steller","Ethereum","Litecoin"]
+    "Select your first coin:",choices=["bitcoin", "uniswap", "steller", "ethereum", "litecoin"]
 ).ask()
 second_coin=questionary.rawselect(
-    "Select your second coin:", choices=["Bitcoin", "Uniswap", "Steller","Ethereum","Litecoin"]
+    "Select your second coin:", choices=["bitcoin", "uniswap", "steller", "ethereum", "litecoin"]
 ).ask()
 # Getting the user's amount of investment.
 investment=questionary.text("How much do you want to invest?").ask()
@@ -93,40 +93,40 @@ first_coin_share=int(questionary.text(f"What percentage of this amount do you wa
 second_coin_share=100-first_coin_share
 answer=questionary.confirm(f"You want to invest {first_coin_share}% of your ${investment} in {first_coin} and {second_coin_share}% in {second_coin}.right?").ask()
 #Setting up the first dataframe to start simulation, based on the user's first choice.
-if first_coin=="Bitcoin":
-    first_coin_df= Bitcoin
-elif first_coin=="Uniswap":
-    first_coin_df=Uniswap
-elif first_coin=="Steller":
-    first_coin_df=Steller
-elif first_coin=="Ethereum":
-    first_coin_df=Ethereum
-elif first_coin=="Litecoin":
-    first_coin_df=Litecoin
+if first_coin=="bitcoin":
+    first_coin_df= bitcoin
+elif first_coin=="uniswap":
+    first_coin_df=uniswap
+elif first_coin=="steller":
+    first_coin_df=steller
+elif first_coin=="ethereum":
+    first_coin_df=ethereum
+elif first_coin=="litecoin":
+    first_coin_df=litecoin
 #Setting up the second dataframe to start simulation, based on the user's second choice.
-if second_coin=="Bitcoin":
-    second_coin_df= Bitcoin
-elif second_coin=="Uniswap":
-    second_coin_df=Uniswap
-elif second_coin=="Steller":
-    second_coin_df=Steller
-elif second_coin=="Ethereum":
-    second_coin_df=Ethereum
-elif second_coin=="Litecoin":
-    second_coin_df=Litecoin
+if second_coin=="bitcoin":
+    second_coin_df= bitcoin
+elif second_coin=="uniswap":
+    second_coin_df=uniswap
+elif second_coin=="steller":
+    second_coin_df=steller
+elif second_coin=="ethereum":
+    second_coin_df=ethereum
+elif second_coin=="litecoin":
+    second_coin_df=litecoin
 #Concatinating the two dataframes to prepare the input for the Monter Carlo simulation
-Two_coin_concat = pd.concat([first_coin_df, second_coin_df], axis=1, keys=[first_coin,second_coin], join="inner")
-MC_Portfolio_weight = MCSimulation(
-    portfolio_data = Two_coin_concat,
+two_coin_concat = pd.concat([first_coin_df, second_coin_df], axis=1, keys=[first_coin,second_coin], join="inner")
+MC_portfolio_weight = MCSimulation(
+    portfolio_data = two_coin_concat,
     weights= [first_coin_share/100,second_coin_share/100],
     num_simulation = 500 ,
     num_trading_days = 3*364
 )
 #Running Monte Carlo simulation
-MC_Portfolio_weight.calc_cumulative_return()
-Portfolio_table= MC_Portfolio_weight.summarize_cumulative_return()
-lower_CI=round(Portfolio_table[8]*int(investment),2)
-upper_CI=round(Portfolio_table[9]*int(investment),2)
+MC_portfolio_weight.calc_cumulative_return()
+portfolio_table= MC_portfolio_weight.summarize_cumulative_return()
+lower_ci=round(portfolio_table[8]*int(investment),2)
+upper_ci=round(portfolio_table[9]*int(investment),2)
 #Giving the result of the simulation to inform the user about the potential benefit of investing in these cryptocurrencies.
 print(f"There is a 95% chance that the profit of an initial investment of ${investment} in the selected portfolio"
-      f" over the next 3 years would be in the range of ${lower_CI} and ${upper_CI}.")
+      f" over the next 3 years would be in the range of ${lower_ci} and ${upper_ci}.")
--- README.md
+++ README.md
@@ -1,13 +1,41 @@
 # UWFinTech_Group3_Project1
-## Analyzing five cryptocurrencies historical prices and providing guidance for investment
-We have developed an appthat works through CLI which gives information about the cumulative return percentage since the beginning of 2021 and Sharpe ratios as an indicator of the risk associated with investing in each one of them. The app then takes the two cryptocurrencies that the user is interested in investing in, the amount of investment and the share of each coin in the portfolio of the investment and based on those information runs the Monte Carlo simulation to return the range of the prospect of investing in those coins after three years.
+
+## Crypto Trend Analysis And Predictions
+
+This project is designed for quantitative analysis used in FinTech investing platform. This project aims to offer clients a one-stop online investment solution for the customer's portfolios. The code and analysis of different graphs and visualization helps in determining whether the cryptocurrencies with the most investment potential based on key risk-management metrics: the daily returns, cummulative Returns, standard deviations, Sharpe ratios.
+
+We have also developed an application that works through CLI which gives information about the cumulative return percentage, Standard Deviation and Sharpe ratios from the beginning of 2021 to till date as an indicator of the risk associated with investing in each one of them. The application is designed to takes the two cryptocurrencies from the user as investment options, the amount of investment and the share of each cryptocurrency in the portfolio of the investment, based on those informations the Monte Carlo simulation is programmed to provide returns and the range of the prospect of investing in those coins after three years.
+
 The list of the coins that the user can choose from is : Bitcoin (BTC), Ethereum (ETH), Litcoin (LTC), Uniswap (UNI), and Steller (XLM)
 
 ---
 
-## Technologies
-We use Python 3.5+ and along with several relevant libraries. We used Pandas which is included in Anaconda. Please go to: https://docs.anaconda.com/anaconda/install/ - For finding the proper installation guide.
+## Technologies Used
+
+Leveraging Jupyter Notebook, Python 3.5+.
+Leveraging the use of Pandas which is included in Anaconda.
+Please go to: https://docs.anaconda.com/anaconda/install/ - For finding the proper installation guide.
 The packages will be:
+Gitbash CLI is used to pull and push the code from local repository to remote repository (GitHub).
+Code written with the help of Jupyter Notebook and VSCode
+
+
+---
+
+## Libraries and Dependencies
+
+import pandas as pd
+import datetime
+import numpy as np
+import cbpro
+import csv
+import questionary
+import hvplot.pandas
+from MCForecastTools import MCSimulation
+
+%matplotlib inline
+
+ 
 * [questionary](https://github.com/tmbo/questionary) - For interactive user prompts and dialogs
 * [pandas](https://github.com/pandas-dev/pandas)- For information and documentation
 * [Numpy](https://github.com/AhmetFurkanDEMIR/Numpy)- For information on the library and insights
@@ -17,30 +45,162 @@ The packages will be:
 
 ## Installation Guide
 Aside from installation of Anaconda, We need to install questionary and then import its library.
-```python
-    pip install questionary
-```
 
+`pip install questionary`
+
+Coinbase Pro installation:
+
+```pip install cbpro
+#or
+
+pip install git+git://github.com/danpaquin/coinbasepro-python.git
+```
 ---
 
+## Analysis 
+
+To analyse the data first import the current data from coinbase pro and prepare your daily returns DataFrame for analysis. 
+
+Example for fetching Bitcoin crypto from coinbase pro:
+
+### Step1: (Initialize the client and fetch historical Data)
+
+Initialize the client, obtain our products data, and put it into a pandas data frame for better observation: 
+#import cbpro import pandas as pd c = cbpro.PublicClient()
+
+fetching historical data from cbpro:
+
+```
+btc_df = pd.DataFrame(c.get_product_historic_rates('BTC-USD', start = '2021-01-01T01:00:00', end = date.today(), granularity = 86400))
+btc_df.columns = ["Date","Open","High","Low","Close","Volume"]
+btc_df['Date'] = pd.to_datetime(btc_df['Date'], unit='s')
+btc_df.set_index('Date', inplace=True)
+btc_df.sort_values(by='Date', ascending=True, inplace=True)
+btc_df
+```
+
+### Step 2: (Slice Data and concat the dataframes)
+After fetching all the required historical concat all the dataframes into one dataframe and drop all the columns except the "Close" column for further analysis:
+
+```
+btc_sliced = btc_df.loc[:, 'Close']
+display(btc_sliced.head())
+eth_sliced = eth_df.loc[:, 'Close']
+display(eth_sliced.head())
+ltc_sliced = ltc_df.loc[:, 'Close']
+display(ltc_sliced.head())
+uni_sliced = uni_df.loc[:, 'Close']
+display(uni_sliced.head())
+xlm_sliced = xlm_df.loc[:, 'Close']
+```
+
+```
+crypto_portfolio_df = pd.concat([btc_sliced, eth_sliced, ltc_sliced, uni_sliced, xlm_sliced], axis=1)
+crypto_portfolio_df.columns = ['BTC', 'ETH', 'LTC', 'UNI', 'XLM']
+```
+
+### Step3: (Performance Analysis:)
+
+Prepare for the analysis by converting the dataframe of closing prices to daily returns
+Drop any rows with all missing values
+Review the first five rows of the daily returns DataFrame.
+
+
+```
+crypto_daily_returns = crypto_portfolio_df.pct_change().dropna()
+crypto_daily_returns.head()
+```
+
+### Step4: (Volatility Analysis & Risk-Return Ratio:)
+
+
+Calculate and plot the cumulative returns of the 5 cryptocurrencies
+Review the last 5 rows of the cumulative returns DataFrame
+
+```crypto_cumulative_returns = (1 + crypto_daily_returns).cumprod()```
+
+Calculate and sort the standard deviation for all 5 crypto portfolios.
+Review the standard deviations sorted smallest to largest
+
+```crypto_std_df = crypto_daily_returns.std().sort_values()```
+
+Calculate and sort the annualized standard deviation (calculate the trading days) of the 5 currencies
+Review the annual standard deviations smallest to largest
+Trading Days 01-01-2021 - current date 
+
+```
+trading_days = crypto_daily_returns.count()[1]+1
+print(f"The Trading days are: {trading_days}")
+crypto_annual_std = crypto_std_df * np.sqrt(trading_days)
+crypto_annual_std.sort_values()
+```
+
+Calculate the annual average return data for the for all the current crypto currencies.
+Review the annual average returns sorted from lowest to highest
+Trading Days 01-01-2021 - till current's date.
+Use the Pandas mean function to calculate the average daily return for 5 cryptocurrencies.
+
+```annual_average_return = crypto_daily_returns.mean() * trading_days```
+
+Sort the annual average returns from lowest to highest
+
+```annual_average_return.sort_values()```
+
+Calculate the annualized Sharpe Ratios for all the cryptocurrencies.
+Review the Sharpe ratios sorted lowest to highest
+
+```
+sharpe_ratio = annual_average_return / crypto_annual_std
+sharpe_ratio.sort_values()
+```
+
+Then, you’ll do a quantitative analysis that includes the following:
+
+
+
 ## Usage
+
 In order to see the graphs of the analysis go to the "Images" folder of the repository and find the Cumulative Return, the Daily Return and the comparative Sharpe Ratio plots of all the five cryptocurrencies.
+
+### CLI Application Usage:
+
+The application is designed to interact with customer and dynamically get the data and simulate the operation and give a outlook of the profit and loss margin based on 95% confidence level. CLI-project_1 is the python file written for the application to run on CLI interface.
+
 To run the app, clone the repository on your PC, open Git Bash in the cloned folder and then type:
 ```console
     python CLI-project_1
 ```
 You will see some information on the cumulative return of the 5 cryptocurrencies and their Sharpe ratios.
-Then the program will ask you to choose two of them. In the next step, you will need to enter the amount of investment and at the last step, you will determine the structure of your portfolio. The program will ask your confirmation of the information, and then it will start simulating the historical data of the prices of these coins over the next three years and based on that, provides some insight on the amount of potential profit of this investment.
+Step1: The program will ask you to choose two of them. 
+
+Step2: In the next step, you will need to enter the amount of investment
+
+Step3: You will determine the structure of your portfolio. The program will ask your confirmation of the information.
+
+Step4: The application will start simulating the historical data of the prices of these coins over the next three years and based on that, provides some insight on the amount of potential profit of this investment.
+
+### Simulation Code:
+
+NOTE: Need to import MCSimulation from MCForecastTools, which program is written for simulating portfolio of customer for 500 simulaions.
 
 ---
+
+Image Snippets:
+
+![Daily Returns][UWFinTech_Group3_Project1/Images/Daily_Return_Interactive_Plot.png]
+![Cumulative Returns][UWFinTech_Group3_Project1/Images/Cumulative_Return_Interactive_Plot.png]
+![Sharpe_Ratio][UWFinTech_Group3_Project1/Images/Sharpe_Ratio_Plot.png]
 
 ## Contributors
-- Somaye Nargesi  
-- Swati Sobhadarshini  
-- Jensen Eichenlaub  
 
----
+This project is designed by - 
 
-## License
+Swati Subhadarshini
+Emaid id: sereneswati@gmail.com
+LinkedIn link: www.linkedin.com/in/swati-subhadarshini
 
-MIT
+Somaye Nargesi
+Email address:srn1358@gmail.com
+
+Jansen Eichenlaub
+
